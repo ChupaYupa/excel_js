51db73646b85b342497611d700ea88f47a1b9ba9
ChupaYupa <docsperj@gmail.com>
2021-03-15 14:16:37 +0300

    add change-text

--- src/components/toolbar/Toolbar.js
+++ src/components/toolbar/Toolbar.js
@@ -1,34 +1,40 @@
-import {ExcelComponent} from '@core/ExcelComponent';
+import {ExcelComponent} from '@core/ExcelComponent'
 
-export class Toolbar extends ExcelComponent{
-    debugger
-    static className = 'excel__toolbar'
-    constructor($root, options) {
-        super($root, {
-            name: 'Toolbar',
-            listeners: [],
-            ...options
-        });
-    }
+export class Toolbar extends ExcelComponent {
+  static className = 'excel__toolbar'
 
-    toHTML() {
-        return `<div class="button">
-                <span class="material-icons">format_align_left</span>
-            </div>
-            <div class="button">
-                <span class="material-icons">format_align_center</span>
-            </div>
-            <div class="button">
-                <span class="material-icons">format_align_right</span>
-            </div>
-            <div class="button">
-                <span class="material-icons">format_bold</span>
-            </div>
-            <div class="button">
-                <span class="material-icons">format_italic</span>
-            </div>
-            <div class="button">
-                <span class="material-icons">format_underlined</span>
-            </div>`
-    }
+  constructor($root, options) {
+    super($root, {
+      name: 'Toolbar',
+      ...options
+    })
+  }
+
+  toHTML() {
+    return `
+      <div class="button">
+        <i class="material-icons">format_align_left</i>
+      </div>
+
+      <div class="button">
+        <i class="material-icons">format_align_center</i>
+      </div>
+
+      <div class="button">
+        <i class="material-icons">format_align_right</i>
+      </div>
+
+      <div class="button">
+        <i class="material-icons">format_bold</i>
+      </div>
+
+      <div class="button">
+        <i class="material-icons">format_italic</i>
+      </div>
+
+      <div class="button">
+        <i class="material-icons">format_underlined</i>
+      </div>
+    `
+  }
 }
--- src/index.js
+++ src/index.js
@@ -6,20 +6,19 @@ import {Table} from '@/components/table/Table'
 import {createStore} from '@core/createStore'
 import {rootReducer} from '@/redux/rootReducer'
 import {storage} from '@core/utils'
+import {initialState} from '@/redux/initialState'
 import './scss/index.scss'
 
-const store = createStore(rootReducer, storage('excel-state'))
+const store = createStore(rootReducer, initialState)
 
 store.subscribe(state => {
-    console.log('App State: ', state)
-    storage('excel-state', state)
+  console.log('App State: ', state)
+  storage('excel-state', state)
 })
 
 const excel = new Excel('#app', {
-    components: [Header, Toolbar, Formula, Table],
-    store
+  components: [Header, Toolbar, Formula, Table],
+  store
 })
 
 excel.render()
-
-
